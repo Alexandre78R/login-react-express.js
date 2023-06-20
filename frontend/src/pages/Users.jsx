51caa1eadc8395e5ece8bacc294a35d823af25b4
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-06-20 19:03:59 +0200

    ajout de l'étape 4 du frontend

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,10 +1,27 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Users() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    // TODO
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/users`, {
+        withCredentials: true,
+      })
+      .then((res) => res.data)
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          alert("You're not authenticated");
+        }
+        if (err.response.status === 403) {
+          alert("You're not authorized");
+        }
+        console.error(err);
+      });
   }, []);
 
   return (
