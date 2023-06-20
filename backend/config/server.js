f4ee5a243d77a48c86959327d630b8cabc22f2b2
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-06-20 17:34:10 +0200

    Ajout de l'étape 5

--- backend/config/server.js
+++ backend/config/server.js
@@ -7,7 +7,13 @@ const router = require('../router.js');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(
+    cors({
+      origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
+      optionsSuccessStatus: 200,
+      credentials: true,
+    })
+  );
 app.use(helmet())
 
 app.use('/', router);
