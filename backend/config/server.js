2685e753826b6e096dc19d31c5111c584604f005
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-06-20 17:58:32 +0200

    etape7

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const app = express();
 const router = require('../router.js');
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -15,6 +16,7 @@ app.use(
     })
   );
 app.use(helmet())
+app.use(cookieParser());
 
 app.use('/', router);
 
